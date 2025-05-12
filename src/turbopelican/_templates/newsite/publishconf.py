726ad80e014eb71ef16918e313cebdc7a0cd3b73
Ilya Simpson <elliot@p-s.co.nz>
2025-05-12 21:53:48 +1200

    Loading config more dependably.
    Process now involves using Pydantic models to validate the provided
    configuration. Also, settings under `pelican` can be used to implicitly
    stand in place of omitted settings in `publish`.

--- src/turbopelican/_templates/newsite/publishconf.py
+++ src/turbopelican/_templates/newsite/publishconf.py
@@ -33,33 +33,30 @@ __all__ = [
     "TRANSLATION_FEED_ATOM",
 ]
 
-from turbopelican import load_config
+from turbopelican import config
 
-_base_config = load_config()
-_config = _base_config.pelican
-_publish_config = _base_config.publish
-
+_config = config("PUBLISH")
 
 ARTICLE_PATHS: list[str] = _config.article_paths
-AUTHOR: str = _config.author
-AUTHOR_FEED_ATOM: None = None
-AUTHOR_FEED_RSS: None = None
-CATEGORY_FEED_ATOM: str = _publish_config.category_feed_atom
+AUTHOR: str | None = _config.author
+AUTHOR_FEED_ATOM: str | None = _config.author_feed_atom
+AUTHOR_FEED_RSS: str | None = _config.author_feed_rss
+CATEGORY_FEED_ATOM: str | None = _config.category_feed_atom
 DEFAULT_LANG: str = _config.default_lang
-DEFAULT_PAGINATION: bool = _config.default_pagination
-DELETE_OUTPUT_DIRECTORY: bool = _publish_config.delete_output_directory
+DEFAULT_PAGINATION: int | bool = _config.default_pagination
+DELETE_OUTPUT_DIRECTORY: bool = _config.delete_output_directory
 EXTRA_PATH_METADATA: dict[str, dict[str, str]] = _config.extra_path_metadata
-FEED_ALL_ATOM: str = _publish_config.feed_all_atom
+FEED_ALL_ATOM: str | None = _config.feed_all_atom
 INDEX_SAVE_AS: str = _config.index_save_as
 LINKS: tuple[tuple[str, str], ...] = _config.links
 PAGE_PATHS: list[str] = _config.page_paths
 PAGE_SAVE_AS: str = _config.page_save_as
 PATH: str = _config.path
-RELATIVE_URLS: bool = _publish_config.relative_urls
+RELATIVE_URLS: bool = _config.relative_urls
 SITENAME: str = _config.sitename
-SITEURL: str = _publish_config.site_url
+SITEURL: str = _config.site_url
 SOCIAL: tuple[tuple[str, str], ...] = _config.social
 STATIC_PATHS: list[str] = _config.static_paths
 THEME: str = _config.theme
 TIMEZONE: str = _config.timezone
-TRANSLATION_FEED_ATOM: None = None
+TRANSLATION_FEED_ATOM: str | None = _config.translation_feed_atom
