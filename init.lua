b5efb4e04f68aee9cb8f1e0af0c6751e1e4f51be
Ilya Simpson <elliot@p-s.co.nz>
2025-05-15 22:50:33 +1200

    Prettifying `init.lua`.
    Using `https://codebeautify.org/lua-beautifier`.

--- init.lua
+++ init.lua
@@ -1,45 +1,51 @@
-local lspconfig = require('lspconfig')
+local lspconfig = require("lspconfig")
 
-lspconfig.ruff.setup({
-  on_attach = function(client, bufnr)
-    vim.api.nvim_create_autocmd("BufWritePre", {
-      buffer = bufnr,
-      callback = function()
-        vim.lsp.buf.format({ bufnr = bufnr })
-      end,
-    })
-  end,
-  init_options = {
-    settings = {
-      ruff = {
-        formatOnSave = true,
-      }
+lspconfig.ruff.setup(
+    {
+        on_attach = function(client, bufnr)
+            vim.api.nvim_create_autocmd(
+                "BufWritePre",
+                {
+                    buffer = bufnr,
+                    callback = function()
+                        vim.lsp.buf.format({bufnr = bufnr})
+                    end
+                }
+            )
+        end,
+        init_options = {
+            settings = {
+                ruff = {
+                    formatOnSave = true
+                }
+            }
+        }
     }
-  }
-})
+)
 
-lspconfig.pyright.setup({
-  capabilities = (function ()
-    local capabilities = vim.lsp.protocol.make_client_capabilities()
-    capabilities.textDocument.publishDiagnostics.tagSupport.valueSet = { 2 }
-    return capabilities
-  end)(),
-  settings = {
-    pyright = {
-      -- Using Ruff's import organizer	
-      disableOrganizeImports = true,
-    },
-    python = {
-      typeCheckingMode = 'strict',
-      analysis = {
-	useLibraryCodeForTypes = true,
-	autoSearchPaths = true,
-	diagnosticMode = "openFilesOnly",
-	diagnosticSeverityOverrides = {
-	  reportUnusedVariable = "warning",
-	},
-      },
-    },
-  },
-})
-
+lspconfig.pyright.setup(
+    {
+        capabilities = (function()
+            local capabilities = vim.lsp.protocol.make_client_capabilities()
+            capabilities.textDocument.publishDiagnostics.tagSupport.valueSet = {2}
+            return capabilities
+        end)(),
+        settings = {
+            pyright = {
+                -- Using Ruff's import organizer
+                disableOrganizeImports = true
+            },
+            python = {
+                typeCheckingMode = "strict",
+                analysis = {
+                    useLibraryCodeForTypes = true,
+                    autoSearchPaths = true,
+                    diagnosticMode = "openFilesOnly",
+                    diagnosticSeverityOverrides = {
+                        reportUnusedVariable = "warning"
+                    }
+                }
+            }
+        }
+    }
+)
