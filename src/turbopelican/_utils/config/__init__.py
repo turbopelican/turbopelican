faad34f2a935600decf9707a1b994416628aae75
Ilya Simpson <elliot@p-s.co.nz>
2025-05-24 11:00:46 +1200

    Separating legacy code that will be removed later.

--- src/turbopelican/_utils/config/__init__.py
+++ src/turbopelican/_utils/config/__init__.py
@@ -2,3 +2,20 @@
 
 Author: Elliot Simpson.
 """
+
+__all__ = [
+    "Configuration",
+    "PelicanConfig",
+    "PelicanConfiguration",
+    "PublishConfiguration",
+    "config",
+    "load_config",
+]
+
+from turbopelican._utils.config.config import PelicanConfig, config
+from turbopelican._utils.config.legacy import (
+    Configuration,
+    PelicanConfiguration,
+    PublishConfiguration,
+    load_config,
+)
