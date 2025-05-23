0bc8e9ed145ebb12f039108718187f1061e39037
Ilya Simpson <elliot@p-s.co.nz>
2025-05-24 10:57:54 +1200

    Isolating configuration/root-finding code.

---
+++ src/turbopelican/_utils/shared/__init__.py
@@ -0,0 +1,11 @@
+"""This package stores code shared by different utilities across Turbopelican.
+
+Author: Elliot Simpson.
+"""
+
+__all__ = [
+    "Toml",
+    "find_config",
+]
+
+from turbopelican._utils.shared.shared import Toml, find_config
